fa827aed177944f35f00aabd1519688ecf8b2411
dcoopennet <dco@open-net.ch>
2016-06-29 09:35:21 +0200

    Add files via upload

---
+++ payment_postfinance_test/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016 Coninckx David (Open Net Sàrl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Postfinance payment',
+    'summary': 'This module add Postfinance for online payment',
+    'category': 'Hidden',
+    'author': "Open Net Sàrl",
+    'depends': [
+        'payment'
+    ],
+    'version': '9.0.1.0.0',
+    'auto_install': False,
+    'website': 'http://open-net.ch',
+    'license': 'AGPL-3',
+    'images': [],
+    'data': [
+    ],
+    'installable': True
+}
