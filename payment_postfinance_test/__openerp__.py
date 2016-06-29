b1757e50800e21d3b433cab99875c1adb680c29a
dcoopennet <dco@open-net.ch>
2016-06-29 16:39:29 +0200

    Update __openerp__.py

--- payment_postfinance_test/__openerp__.py
+++ payment_postfinance_test/__openerp__.py
@@ -6,7 +6,7 @@
     'depends': [
         'payment'
     ],
-    'version': '1.0.0',
+    'version': '1.0.2',
     'auto_install': False,
     'website': 'http://open-net.ch',
     'license': 'AGPL-3',
